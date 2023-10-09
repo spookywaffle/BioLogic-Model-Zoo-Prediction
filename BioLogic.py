d1e12d6876a6842095aa8f147daebab3fb23724f
AryanC23 <110075130+AryanC23@users.noreply.github.com>
2023-10-09 03:12:38 -0600

    Update BioLogic.py

--- BioLogic.py
+++ BioLogic.py
@@ -9,15 +9,30 @@ from tensorflow.keras.metrics import Accuracy
 
 data = pd.read_csv('nasa_animal_testing_data.csv')
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+data.dropna(inplace=True)
+X = data.drop(columns=['target_column'])
+y = data['target_column']
+
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_val_scaled = scaler.transform(X_val)
+X_test_scaled = scaler.transform(X_test)
 
 model = Sequential()
-model.add(LSTM(64, input_shape=(sequence_length, num_features)))
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(64, activation='relu', input_dim=X_train_scaled.shape[1]))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
-model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=[Accuracy()])
-model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=32)
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=[Accuracy()])
 
-new_data = pd.read_csv('new_nasa_animal_testing_data.csv')
-X_new = preprocess(new_data)
-predictions = model.predict(X_new)
+model.fit(X_train_scaled, y_train, validation_data=(X_val_scaled, y_val), epochs=10, batch_size=32)
+
+evaluation_results = model.evaluate(X_test_scaled, y_test)
+print("Test Loss:", evaluation_results[0])
+print("Test Accuracy:", evaluation_results[1])
+
+X_new_scaled = scaler.transform(new_data)
+predictions = model.predict(X_new_scaled)
